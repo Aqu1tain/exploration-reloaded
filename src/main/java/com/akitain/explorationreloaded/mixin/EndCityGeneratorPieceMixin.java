af9d379f062be98accba93c971d1b7a352027a29
Corentin Renard <corentinfox08@gmail.com>
2026-05-18 10:08:15 +0200

    fix(mojmap): update end city ship template lookup

--- src/main/java/com/akitain/explorationreloaded/mixin/EndCityGeneratorPieceMixin.java
+++ src/main/java/com/akitain/explorationreloaded/mixin/EndCityGeneratorPieceMixin.java
@@ -2,26 +2,26 @@ package com.akitain.explorationreloaded.mixin;
 
 import com.akitain.explorationreloaded.ExplorationReloaded;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.levelgen.structure.structures.EndCityPieces;
 import net.minecraft.world.level.storage.loot.LootTable;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(EndCityPieces.EndCityPiece.class)
-public class EndCityGeneratorPieceMixin {
+public abstract class EndCityGeneratorPieceMixin {
+    private static final Identifier END_SHIP_TEMPLATE = Identifier.withDefaultNamespace("end_city/ship");
     private static final ResourceKey<LootTable> END_SHIP_LOOT_TABLE = ResourceKey.create(Registries.LOOT_TABLE, ExplorationReloaded.id("end_ship_loot"));
 
     @Shadow
-    @Final
-    protected String templateName;
+    protected abstract Identifier makeTemplateLocation();
 
     @ModifyArg(method = "handleDataMarker", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/RandomizableContainer;setBlockEntityLootTable(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/util/RandomSource;Lnet/minecraft/core/BlockPos;Lnet/minecraft/resources/ResourceKey;)V"), index = 3)
     private ResourceKey<LootTable> useEndShipLootTable(ResourceKey<LootTable> lootTable) {
-        if (!"ship".equals(this.templateName)) {
+        if (!END_SHIP_TEMPLATE.equals(this.makeTemplateLocation())) {
             return lootTable;
         }
 
