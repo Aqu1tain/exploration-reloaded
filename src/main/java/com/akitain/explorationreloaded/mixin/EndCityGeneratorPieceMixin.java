f8aaec5fbfc86017d75627c1c1ded4433c774a07
Corentin Renard <corentinfox08@gmail.com>
2026-06-12 23:08:15 +0200

    fix: update end city ship template lookup

--- src/main/java/com/akitain/explorationreloaded/mixin/EndCityGeneratorPieceMixin.java
+++ src/main/java/com/akitain/explorationreloaded/mixin/EndCityGeneratorPieceMixin.java
@@ -5,23 +5,23 @@ import net.minecraft.loot.LootTable;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.structure.EndCityGenerator;
-import org.spongepowered.asm.mixin.Final;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(EndCityGenerator.Piece.class)
-public class EndCityGeneratorPieceMixin {
+public abstract class EndCityGeneratorPieceMixin {
+    private static final Identifier END_SHIP_TEMPLATE = Identifier.ofVanilla("end_city/ship");
     private static final RegistryKey<LootTable> END_SHIP_LOOT_TABLE = RegistryKey.of(RegistryKeys.LOOT_TABLE, ExplorationReloaded.id("end_ship_loot"));
 
     @Shadow
-    @Final
-    protected String templateIdString;
+    protected abstract Identifier getId();
 
     @ModifyArg(method = "handleMetadata", at = @At(value = "INVOKE", target = "Lnet/minecraft/inventory/LootableInventory;setLootTable(Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/random/Random;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/registry/RegistryKey;)V"), index = 3)
     private RegistryKey<LootTable> useEndShipLootTable(RegistryKey<LootTable> lootTable) {
-        if (!"ship".equals(this.templateIdString)) {
+        if (!END_SHIP_TEMPLATE.equals(this.getId())) {
             return lootTable;
         }
 
